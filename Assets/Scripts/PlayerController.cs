a7e39ba9099989f26cb9ea8caa1752c9eda851ae
PottyPirate <tpotter98@icloud.com>
2022-05-24 20:41:45 +0100

    add player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,43 +2,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
+    private CharacterController cc;
     private float mouseX, mouseY;
 
     public float xSensitivity, ySensitivity, moveSpeed;
     public Camera playerCamera;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mouseX, mouseY, moveForward, moveRight;
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
 
         transform.Rotate(new Vector3(0, mouseX * xSensitivity, 0));
         playerCamera.transform.Rotate(new Vector3(- mouseY * ySensitivity, 0, 0));
 
-        Vector3 velocityChange = transform.forward * 100;
-        rb.velocity = new Vector3(
-            moveSpeed * transform.forward.x,
-            moveSpeed * transform.forward.y,
-            moveSpeed * transform.forward.z);
+        moveForward = Input.GetAxis("Vertical");
+        moveRight = Input.GetAxis("Horizontal");
+        print(moveForward.ToString() + ", " + moveRight.ToString());
 
-        /*if (input.getkey(keycode.w))
-        {
-            rb.velocity += transform.forward * movespeed;
-        }
-        else if (input.getkey(keycode.d))
-        {
-            rb.velocity += transform.right * movespeed;
-        }
-        else if (input.getkey(keycode.s))
-        {
-            rb.velocity -= transform.forward * movespeed;
-        }
-        else if (input.getkey(keycode.a))
-        {
-            rb.velocity -= transform.right * movespeed;
-        }*/
-
+        cc.Move(new Vector3(0, moveRight * transform.right.y * moveSpeed, moveForward * transform.forward.z * moveSpeed) * Time.deltaTime);
     }
 }
